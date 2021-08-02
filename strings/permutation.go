df74aa4de5ed2faa5cbe46bb14a7eaf1aa372367
leif.wang <leif.wang@cardinfolink.com>
2021-08-02 17:06:09 +0800

    feat: string find combination

--- strings/permutation.go
+++ strings/permutation.go
@@ -1,5 +1,9 @@
 package strings
 
+import (
+	"fmt"
+)
+
 // 打印字符串的所有排列
 func Permutation(str string) {
 	permutation([]byte(str), 0)
@@ -21,5 +25,33 @@ func permutation(str []byte, index int) {
 		data[index] = temp
 
 		permutation(data, index+1)
+	}
+}
+
+// 打印字符串的所有组合
+// n 个字符中 m 长度的组合
+// 1个字符和 n-1 个字符
+// 剩余的 n-1 个字符选取 m 个字符
+// 剩余 n-1 个字符选取 m-1 个
+func Combination(str string) {
+	l := len(str)
+	// n 个字符 有1-n种情况
+	for i := 1; i <= l; i++ {
+		combination(str, "", i)
+	}
+}
+
+func combination(str, curStr string, count int) {
+	if count == 0 {
+		fmt.Println(curStr)
+		return
+	}
+
+	curStr += str[:1]
+	count--
+	combination(str[1:], curStr, count)
+
+	if count < len(str)-1 {
+		combination(str[1:], curStr[:len(curStr)-1], count+1)
 	}
 }
--- strings/permutation_test.go
+++ strings/permutation_test.go
@@ -1,6 +1,10 @@
 package strings
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestPermutation(t *testing.T) {
 	s := "abc"
@@ -17,17 +21,88 @@ func BenchmarkPermutation(b *testing.B) {
 //BenchmarkPermutation
 //BenchmarkPermutation-4                 5         202738404 ns/op        99764934 B/op    6235301 allocs/op
 
+func TestCombination(t *testing.T) {
+	s := "abc"
+	Combination(s)
+	//a
+	//b
+	//c
+	//ab
+	//ac
+	//bc
+	//abc
+}
+
+// 切片测试，底层数组不变的情况下，slice值改变的影响
 func TestSlicePoint(t *testing.T) {
 	// 保存的是指针
 	a := []byte("ddd")
 	// 同一个指针
 	b := a
 	b[1] = 'c'
-	t.Log(string(a), string(b))
+	assert.Equal(t, a, b)
+	// dcd
 
 	// 不同的指针
 	c := make([]byte, len(b))
 	copy(c, b)
 	c[1] = 'a'
-	t.Log(string(c), string(b))
+	assert.NotEqual(t, b, c)
+	// b: dcd
+	// c: dad
+
+	d := make([]byte, len(c))
+	copy(d, c)
+	sliceIndexChangeData(c)
+	assert.NotEqual(t, c, d)
+	// c: 2ad
+	// d: dad
+
+	e := make([]byte, len(d))
+	copy(e, d)
+	sliceAppendChangeData(d)
+	assert.Equal(t, d, e)
+	// dad
+
+	sliceChangePointAppendChangeData(&d)
+	assert.Equal(t, d, e)
+	// dad
+
+	slicePointAppendChangeData(&d)
+	assert.NotEqual(t, d, e)
+	// d: ddad
+	// e: dad
+}
+
+func sliceIndexChangeData(s []byte) {
+	if len(s) == 0 {
+		return
+	}
+	s[0] = '2'
+}
+
+func sliceAppendChangeData(s []byte) {
+	if len(s) == 0 {
+		return
+	}
+	s = append(s, 'd')
+}
+
+func slicePointAppendChangeData(s *[]byte) {
+	if s == nil ||
+		len(*s) == 0 {
+		return
+	}
+	// 改变的是指针指向的地址的值
+	*s = append(*s, 'd')
+}
+
+func sliceChangePointAppendChangeData(s *[]byte) {
+	if s == nil ||
+		len(*s) == 0 {
+		return
+	}
+	temp := append(*s, 'd')
+	// 换了一个新指针值
+	s = &temp
 }
